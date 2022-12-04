e4bee780c4a28e0e67a96fb6a15977cbf7434f67
Gabor A <galbitz@gmail.com>
2022-12-04 00:26:06 -0500

    Extend playbook to all vms

--- playbooks/run.sh
+++ playbooks/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-op user list
-if [ $? -ne 0 ]; then
-    echo 'must sign in first with eval $(op signin)'
-    exit
-fi
-
 export ANSIBLE_CONFIG=ansible.cfg
 
 ansible-galaxy install -r roles/requirements.yml
-ansible-playbook docker-server.yml
+ansible-playbook homelab.yml
