8192ec213d23310818674b2c3f68c0a6949ddb82
Gabor A <galbitz@gmail.com>
2022-12-11 00:56:34 -0500

    Extracted common roles to collection

--- playbooks/run.sh
+++ playbooks/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ANSIBLE_CONFIG=ansible.cfg
 
-ansible-galaxy install -r roles/requirements.yml
+ansible-galaxy install -r roles/requirements.yml --force
 ansible-playbook homelab.yml
