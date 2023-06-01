3dcefdbb4c957fae9c6145a69e04930af504b591
Gabor Albitz <gabor@derive.ca>
2023-05-31 21:55:15 -0400

    fix filename

--- decrypt.sh
+++ decrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output my_secret.json encrypted_secrets.sh
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output unencrypted_secrets.sh encrypted_secrets.sh
