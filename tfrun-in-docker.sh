d09f914d60bf0fe0111845dda97b05da458b7b72
Gabor Albitz <gabor@derive.ca>
2023-06-02 19:33:24 -0400

    passing homeip to container

--- tfrun-in-docker.sh
+++ tfrun-in-docker.sh
@@ -8,5 +8,6 @@ docker run --rm \
     -v $(pwd):/tf \
     -w /tf \
     -u $(id -u):$(id -g) \
+    -e TF_VAR_HOME_IP=$TF_VAR_HOME_IP \
     --env-file unencrypted_secrets.sh \
     hashicorp/terraform:1.4 $*
