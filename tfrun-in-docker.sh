3d4965b5516b33d385d6d1e636249fe0f2c5b97e
Gabor Albitz <gabor@derive.ca>
2023-06-02 14:36:03 -0400

    fixing minor issues

--- tfrun-in-docker.sh
+++ tfrun-in-docker.sh
@@ -6,5 +6,7 @@ source unencrypted_secrets.sh
 
 docker run --rm \
     -v $(pwd):/tf \
+    -w /tf \
+    -u $(id -u):$(id -g) \
     -e TF_TOKEN_app_terraform_io=$TF_TOKEN_app_terraform_io \
     hashicorp/terraform:1.4 $*
