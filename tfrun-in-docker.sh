0a7160c0f9f053c0b463e3e4fdbaa0b4439c248f
Gabor Albitz <gabor@derive.ca>
2023-06-02 14:14:09 -0400

    no tty

--- tfrun-in-docker.sh
+++ tfrun-in-docker.sh
@@ -4,7 +4,7 @@ export TF_VAR_HOME_IP=`curl -sS https://ifconfig.co`
 
 source unencrypted_secrets.sh
 
-docker run -it --rm  \
+docker run -it --rm \
     -v $(pwd):/tf \
     -e TF_TOKEN_app_terraform_io=$TF_TOKEN_app_terraform_io \
     hashicorp/terraform:1.4 $*
