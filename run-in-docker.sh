2ab20943714c800c6a2651568a23ee60dde16a08
Gabor Albitz <gabor@derive.ca>
2023-06-03 22:16:25 -0400

    Replaced github pub key request method

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -5,4 +5,4 @@ docker run --rm \
     -w /ansible/playbooks \
     -u $(id -u):$(id -g) \
     --env-file unencrypted_secrets.sh \
-    galbitz/ansible:latest bash -c "curl https://github.com/galbitz.keys | tee -a ~/.ssh/authorized_keys && ./run.sh $*"
+    galbitz/ansible:latest bash -c "ssh-import-id-gh galbitz && ./run.sh $*"
