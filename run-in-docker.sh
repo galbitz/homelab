83b713085817cb62c5ae340d6307e533c736471a
Gabor Albitz <gabor@derive.ca>
2023-06-04 10:25:29 -0400

    No user mapping for ssh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -3,6 +3,5 @@
 docker run --rm \
     -v $(pwd):/ansible \
     -w /ansible/playbooks \
-    -u $(id -u):$(id -g) \
     --env-file unencrypted_secrets.sh \
     galbitz/ansible:latest bash -c "ssh-import-id-gh galbitz && ./run.sh $*"
