0434c0138358d6785bd07c17bc32b1123a9f7070
Gabor Albitz <gabor@derive.ca>
2023-06-04 11:54:17 -0400

    fix line end

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -7,6 +7,6 @@ docker run --rm \
     -v $(pwd):/ansible \
     -v "$SSH_AUTH_SOCK:$SSH_AUTH_SOCK"
     -w /ansible/playbooks \
-    -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK
+    -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
     --env-file unencrypted_secrets.sh \
     galbitz/ansible:latest bash -c "ssh-import-id-gh galbitz && ./run.sh $*"
