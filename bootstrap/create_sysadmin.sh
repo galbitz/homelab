7860e6d772eb3fae88f8a7278ac6fe489bc80d32
Gabor Albitz <gabor@derive.ca>
2024-01-23 22:43:33 -0500

    feature: bootstrap script to create the new management user

---
+++ bootstrap/create_sysadmin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script with elevated privileges (sudo)."
+    exit 1
+fi
+
+username=sysadmin
+
+useradd -m -s /bin/bash"$username"
+usermod -aG sudo "$username"
+
+echo "$username ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/"$username"
+
+sudo -u "$username" mkdir -p /home/"$username"/.ssh
+sudo -u "$username" chmod 700 /home/"$username"/.ssh
+curl -sSL "https://github.com/galbitz.keys" | sudo -u "$username" tee -a /home/"$username"/.ssh/authorized_keys > /dev/null
